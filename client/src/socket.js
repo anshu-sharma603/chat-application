25000441de2c9f155215ec3638637c9974bbdc6e
anshu-sharma603 <anshusharma8981@gmail.com>
2026-09-22 11:05:35 +0530

    Use env-based backend URLs for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export function connectSocket(token) {
   if (socket) return socket;
-  socket = io("http://localhost:5000", {
+  socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000", {
     auth: { token },
   });
   return socket;
